5df7e9d35bea07f1053fffe997d75ae9cd7b623e
Ademola <ademolaogunmokun@yahoo.com>
2022-06-26 12:58:12 +0100

    env test

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const notesRouter = require('./controller/noteController')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
 
-logger.info('connecting to', config.MONGODB_URI)
+const app = express();
+const notesRouter = require('./controller/noteController');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+
+logger.info('connecting to', config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info('connected to MongoDB');
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/notes', notesRouter)
+app.use('/api/notes', notesRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
