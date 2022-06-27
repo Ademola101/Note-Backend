3cba064bf0ec4a848996fdc0983a55758e85a0be
Ademola <ademolaogunmokun@yahoo.com>
2022-06-27 23:30:22 +0100

    beforeEach

--- test/note_api.test.js
+++ test/note_api.test.js
@@ -1,23 +1,44 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
-
+const Note = require('../models/note');
 
 const api = supertest(app);
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  noteObject.save();
+});
 
 test('note return as json', async () => {
   await api.get('/api/notes').expect(200).expect('Content-Type', /application\/json/);
 }, 100000);
 
-test('there are two notes', async () => {
+test('all notes return', async () => {
   const response = await api.get('/api/notes');
-  expect(response.body).toHaveLength(2);
+  expect(response.body).toHaveLength(initialNotes.length);
 }, 100000);
 
-test('the first note is about hello', async () => {
+test('a specific note is within the return note', async () => {
   const response = await api.get('/api/notes');
-  const firstNote = response.body[0].content;
-  expect(firstNote).toBe('Hello word');
+  const content = response.body.map((r) => r.content);
+  expect(content).toContain('Browser can execute only Javascript');
 }, 100000);
 afterAll(() => {
   mongoose.connection.close();
