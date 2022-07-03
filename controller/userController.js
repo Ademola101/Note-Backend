0e7a8b4c6e6edb9300e5d87959f3042eaa08cc0e
Ademola <ademolaogunmokun@yahoo.com>
2022-07-03 14:04:26 +0100

    short cut

--- controller/userController.js
+++ controller/userController.js
@@ -1,24 +1,23 @@
 const userRouter = require('express').Router();
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); 
 const User = require('../models/user');
 
 userRouter.post('/', async (req, res) => {
   const { username, name, password } = req.body;
   const existingUser = await User.findOne({ username });
   if (existingUser) {
-    return res.status(409).json({ error: 'username already taken' });
+    res.status(409).json({ error: 'username already taken' });
   }
   const saltround = 10;
   const passwordHash = await bcrypt.hash(password, saltround);
-  const newUser = new User({
+  const newUser = await User.create({
     username,
     name,
     passwordHash,
   });
 
-  const savedUser = await newUser.save();
-  res.status(201).json(savedUser);
+  res.status(201).json(newUser);
 });
 
 userRouter.get('/', async (req, res) => {
